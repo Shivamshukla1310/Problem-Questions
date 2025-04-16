cd8edac0a9a67039c722a9f0148e498b4ad7429a
Shivam Shukla <shukla.shivamv@gmail.com>
2025-04-16 14:48:37 +0530

    Smallest element finding done

--- Problem Solving Questions/Find_Largest_Number.java
+++ Problem Solving Questions/Find_Largest_Number.java
@@ -7,19 +7,24 @@ class Find_Largest_Number{
         int n = sc.nextInt();
         int a[] = new int[n];
         for(int i = 0; i < n; i++){
-            int val = sc.nextInt();
-            a[i] = val;
+            a[i] = sc.nextInt();
         }
-
+        int smallest = a[0];
         for(int i = 0; i < a.length; i++){
-            for(int j = 0; j < a.length; j++){
-                if(a[i] > a[j]){
-                    a[i] = a[j];
-                }
+            if(a[i] < a[0]){
+                smallest = i;
             }
-
         }
-        int largest = a.length;
-        System.out.print(a[largest]);
+        System.out.print("Smallest index: " + smallest);
+        // for(int i = 0; i < n; i++){
+        //     for(int j = 0; j < i; j++){
+        //         if(a[i] > a[j]){
+        //             a[i] = a[j];
+        //         }
+        //     }
+
+        // }
+        // int largest = a.length;
+        // System.out.print(a[largest]);
     }
 }
