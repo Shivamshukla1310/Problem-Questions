99a20dc97acee752302ede44b9d0daf6d03612ef
Shivam Shukla <shukla.shivamv@gmail.com>
2025-04-16 13:59:47 +0530

    Array Reversing

---
+++ Problem Solving Questions/Array_Reverse.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Array_Reverse{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int a = sc.nextInt();
+    }
+}
