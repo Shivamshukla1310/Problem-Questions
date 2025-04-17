80d7b4e3b33100a737b74f154a9ef28d147ab7c1
Shivam Shukla <shukla.shivamv@gmail.com>
2025-04-17 15:20:05 +0530

    Programs Updated

--- Problem Solving Questions/k_th_bit.java
+++ Problem Solving Questions/k_th_bit.java
@@ -11,8 +11,17 @@ class k_th_bit{
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Any Number: ");
         int a = sc.nextInt();
-        System.out.print("Enter K th Element: ");
-        int k = sc.nextInt();
+        // System.out.print("Enter K th Element: ");
+        // int k = sc.nextInt();
+        String Binary_num = Integer.toBinaryString(a);
         System.out.println("Binary Number: " + Integer.toBinaryString(a));
+        int temp = 0;
+        int count = 0;
+        for(int i = 0; i < Binary_num.length(); i++){
+            if(Binary_num.charAt(i) == '1'){
+                count += 1;
+            }
+        }
+        System.out.print("\nCount: " + count);
     }
 }
