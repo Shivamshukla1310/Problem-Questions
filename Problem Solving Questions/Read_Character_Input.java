f8ea25888813f8f76c3fb72459b3b27673500a74
Shivam Shukla <shukla.shivamv@gmail.com>
2025-04-18 14:58:02 +0530

    Changes applied but still needs imporvement

--- Problem Solving Questions/Read_Character_Input.java
+++ Problem Solving Questions/Read_Character_Input.java
@@ -3,17 +3,15 @@ import java.util.Scanner;
 class Read_Character_Input{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        // char ch[] = new ch[];
-        // char check = '$'
-        char ch;
         int count = 0;
         while(true){
-            ch = sc.next().charAt(0);
+            char ch = sc.next().charAt(0);
             if(ch == '$'){
                 break;
             }
             count += 1;
         }
         System.out.print(count);
+        sc.close();
     }
 }
