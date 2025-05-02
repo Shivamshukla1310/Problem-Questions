dc3949bf43232c801f3dd449b97122aa8ebb6ec6
Shivam Shukla <shukla.shivamv@gmail.com>
2025-05-02 17:32:36 +0530

    Reverse Array PRogram Added

--- Problem Solving Questions/Array_Reverse.java
+++ Problem Solving Questions/Array_Reverse.java
@@ -4,6 +4,17 @@ class Array_Reverse{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Number: ");
-        int a = sc.nextInt();
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+
+
+        System.out.println("Enter Array Elements: ");
+        for(int i = n - 1; i > 0; i--){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + "");
+        }
     }
 }
