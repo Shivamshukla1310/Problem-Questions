8481018fd2a8e993243d2159f6258d2cd26ac1b9
Shivam Shukla <shukla.shivamv@gmail.com>
2025-05-07 22:18:30 +0530

    Armstrong Array PRogram in process

--- Problem Solving Questions/Array_Armstrong.java
+++ Problem Solving Questions/Array_Armstrong.java
@@ -5,9 +5,14 @@ class Array_Armstrong{
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int arr[] = new int[n];
+        int armstrong = 0;
         System.out.println("Enter Elements: ");
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < n; i++){
+            armstrong = 
         }
     }
 }
